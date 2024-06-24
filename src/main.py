fe57e1205e5ee0ea0552448e6e617df093442ad6
lshiromizu <l.shiromizu@gmail.com>
2024-06-24 16:39:57 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,33 +9,8 @@ def main():
 
     # -------- IMPLEMENT ALGORITHM HERE --------
 
-    device.set_power(15)
-    
-    device.set_antenna(0,0,1,1)
-    
-    res = device.read_start(10)
 
-    device.read_stop()
 
-    df =pd.DataFrame(res)
-
-    res_fltr=[]
-    for i in range(len(res)):
-        for j in range(len(res_fltr)):
-            data= res[i[1]]
-            if res[i[1]]!=res_fltr[j[1]]:
-                res_fltr.append(i)
-                if (len(res_fltr[j][1])==3):
-                    (res_fltr[i]).append(1)
-                else:
-                    (res_fltr[3]) = (res_fltr[3]+1)
-    print(df)
-    
-    print(res_fltr)
-    
-    #device.read_stop()
-    
-    #print("Length:", len(res))
     # ------------------------------------------
 
     
